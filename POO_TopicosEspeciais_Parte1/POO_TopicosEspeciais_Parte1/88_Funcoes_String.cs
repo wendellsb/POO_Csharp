1a4e44a5396cb6489761f6b653b1a0b76f6477c4
Wendell Santos <88899145+wendellsb@users.noreply.github.com>
2022-04-22 10:09:27 -0300

    DataTime

---
+++ POO_TopicosEspeciais_Parte1/POO_TopicosEspeciais_Parte1/88_Funcoes_String.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_TopicosEspeciais_Parte1
+{
+    public class _88_Funcoes_String
+    {
+        static void Main(string[] args)
+        {
+            string original = "abcde FGHIJ ABC abc DEFG  ";
+            // transforma os caracteres em maiusculos
+            string s1 = original.ToUpper();
+
+            // transforma os caracteres em minusculos
+            string s2 = original.ToLower();
+
+            // apaga os espaços depois e antes da string
+            string s3 = original.Trim();
+
+            // busca dentro da string a primeira ocorencia
+            int n1 = original.IndexOf("bc");
+
+            // busca dentro da string a ultima ocorrencia
+            int n2 = original.LastIndexOf("bc");
+
+            // recorta a string a partir do numero 3
+            string s4 = original.Substring(3);
+
+            // recorta 5 caracteres a partir da posição 3
+            string s5 = original.Substring(3, 5);
+
+            // substitui todo caracter 'a' pelo caracter 'x'
+            string s6 = original.Replace('a', 'x');
+            // substitui todo caracter 'a' pelo caracter 'x'
+            string s7 = original.Replace("abc", "xy");
+            
+            // testa se a string é nula ou vazia
+            bool b1 = String.IsNullOrEmpty(original);
+
+            // testa se a variavel é nula oou tem espaços em branco
+            bool b2 = String.IsNullOrWhiteSpace(original);
+
+            // split - recorta uma string com base no caracter
+            // int x = int.Parse(str) - converte string para inteiro
+
+            Console.WriteLine("Original: " + original);
+            Console.WriteLine("ToUpper: " + s1);
+            Console.WriteLine("ToLower: " + s2);
+            Console.WriteLine("Trim: " + s3);
+            Console.WriteLine("IndexOf('bc'): " + n1);
+            Console.WriteLine("LastIndexOf('bc'): " + n2);
+            Console.WriteLine("Substring(3): " + s4);
+            Console.WriteLine("Substring(3, 5): " + s5);
+            Console.WriteLine("Repalce('a', 'x'): " + s6);
+            Console.WriteLine("Repalce('abc', 'xy'): " + s7);
+            Console.WriteLine("IsNullOrEmpty: " + b1);
+            Console.WriteLine("IsNullOrWhiteSpace: " + b1);
+        }
+    }
+}
---
+++ POO_TopicosEspeciais_Parte1/POO_TopicosEspeciais_Parte1/89_DateTime.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_TopicosEspeciais_Parte1
+{
+    public class _89_DateTime
+    {
+        static void Main(string[] args)
+        {
+            // construtor 
+            // d1 = new DateTime(ano, mes, dia);
+            DateTime d1 = new DateTime(2018, 11, 25);
+            // d2 = new DateTime(ano, mes, dia, hora, minuto, segundo);
+            DateTime d2 = new DateTime(2018, 11, 25, 20, 45, 3);
+            // d3 = new DateTime(ano, mes, dia, hora, minuto, segundo, milissegundo);
+            DateTime d3 = new DateTime(2018, 11, 25, 20, 45, 3 , 500);
+            
+            // Builders
+            // data e horario atual
+            DateTime d4 = DateTime.Now;
+            // data de hora com 00 horas
+            DateTime d5 = DateTime.Today;
+            // data e hora com o padrão UTC (3 horas adiantado)
+            DateTime d6 = DateTime.UtcNow;
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+            Console.WriteLine(d5);
+            Console.WriteLine(d6);
+
+            // passando a data por uma string
+            DateTime d7 = DateTime.Parse("2000-08-15");
+            // data e hora por string
+            DateTime d8 = DateTime.Parse("2000-08-15 13:05:58");
+
+            DateTime d9 = DateTime.Parse("2000/08/15");
+            DateTime d10 = DateTime.Parse("2000/08/15 13:05:58");
+
+            DateTime d11 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime d12 = DateTime.ParseExact("2000-08-15 13:05:58", "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+           
+            Console.WriteLine(d7);
+            Console.WriteLine(d8);
+            Console.WriteLine(d9);
+            Console.WriteLine(d10);
+            Console.WriteLine(d11);
+            Console.WriteLine(d12);
+        }
+    }
+}
