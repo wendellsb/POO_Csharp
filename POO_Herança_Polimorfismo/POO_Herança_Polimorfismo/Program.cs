fb38beeb3427a47eb9353f4529a8d2a8c134917a
Wendell Santos <88899145+wendellsb@users.noreply.github.com>
2022-05-19 16:18:56 -0300

    19/05

---
+++ POO_Herança_Polimorfismo/POO_Herança_Polimorfismo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POO_Herança_Polimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
